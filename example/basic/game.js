066ee9770f580dc0e994f864e305b49ef96e8788
Ashwani Rathee <ashwanirathee.work@gmail.com>
2025-07-04 10:39:53 -0700

    Deployed 9c0ddb7 with MkDocs version: 1.6.1

---
+++ example/basic/game.js
@@ -0,0 +1,61 @@
+import * as VoxelMiner from "../../lib/webgl/index.js";
+import { CameraManager } from "./camera-manager.js";
+import { SceneManager } from "./scene-manager.js";
+import { RenderManager } from "./render-manager.js";
+import { InputManager } from "./input-manager.js";
+import GUI from 'https://cdn.jsdelivr.net/npm/lil-gui@0.20/+esm';
+
+
+export const { canvas, gl } = VoxelMiner.setupWebGL();
+export const gui = new GUI();
+
+export var obj = {
+  help: function () { alert('Hi! You might need to click on the screen to get a pointer lock which will allow you to move around in the world. Both keyboard and mouse provide controls!. Hope you like it!') },
+  show_normals: false,
+
+  animate_light: true,
+  light1_color: [1.0, 1.0, 1.0],
+  ambientLightFactor: 0.8,
+  diffuseLightFactor: 0.5,
+  specularLightFactor: 0.2,
+  specularExponent: 16,
+
+  camera_type: "FPS Camera",
+  camera_fov: 45,
+  camera_near: 0.1,
+  camera_far: 100,
+  camera_speed: 2,
+  camera_sensitivity: 0.05,
+  camera_aspect: 1,
+  camera_eye: [13.0, 3.0, -3.0],
+  camera_at: [-0.01, 3, -0.01],
+  camera_up: [0, 1, 0],
+
+  frame_rate: 0,
+  frame_time: 0,
+  draw_calls: 0,
+
+  fallOffStart: 1.0,
+  fallOffEnd: 0.0,
+  fallOffStepExponent: 2.0,
+}
+
+/**
+ * Represents a game.
+ */
+export class Game {
+  constructor() {
+    this.sceneManager = new SceneManager();
+    this.cameraManager = new CameraManager(this.sceneManager.scene);
+    this.renderManager = new RenderManager(this.sceneManager, this.cameraManager);
+    this.inputManager = new InputManager(this.sceneManager, this.cameraManager);
+    VoxelMiner.resizeCanvas(this.cameraManager.camera, this.sceneManager.scene, canvas, gl);
+  }
+
+  /**
+   * Initializes the game.
+   */
+  init() {
+    requestAnimationFrame(this.renderManager.render.bind(this.renderManager));
+  }
+}
---
+++ example/basic/scene-manager.js
@@ -0,0 +1,101 @@
+import * as VoxelMiner from "../../lib/webgl/index.js";
+import { gl, obj } from "./game.js";
+import alea from 'https://cdn.jsdelivr.net/npm/alea@1.0.1/+esm';
+import { createNoise3D, createNoise2D } from 'https://cdn.jsdelivr.net/npm/simplex-noise@4.0.3/+esm';
+const debugkey = "scene_manager";
+
+VoxelMiner.debugLog(debugkey, "Loading scene_manager...");
+
+/**
+ * Represents a scene manager that manages the scene graph, objects, skybox, and lights.
+ */
+export class SceneManager {
+  constructor() {
+    console.log(obj);
+    this.scene = new VoxelMiner.SceneGraph(gl, obj.ambientLightFactor, obj.diffuseLightFactor, obj.specularLightFactor, obj.specularExponent);
+    this.scene.init();
+    this.ball = null;
+    this.addObjects();
+    this.addSkyBox();
+    this.addLights();
+    this.addCrosshair();
+  }
+
+  /**
+   * Adds objects to the scene graph.
+   */
+  addObjects() {
+    const prng = alea('seed');
+    const noise3D = createNoise3D(prng);
+    const noise2D = createNoise2D(prng);
+    VoxelMiner.debugLog(debugkey, "Adding objects to scene graph");
+
+    // Generate noise for some x, y coordinates
+    for (let y = -15; y < 0; y++) {
+      for (let x = -15; x < 15; x++) {
+        for (let z = -15; z < 15; z++) {
+          // Generate noise for the current x, y, z coordinates
+          const noiseValue = noise3D(x / 10, y / 10, z / 10);
+          if (noiseValue > 0.2) {
+            let cube = new VoxelMiner.Cube(2, 3, gl);
+            cube.color = [0.49, 0.788, 0.29, 1.0];
+            cube.matrix.translate(x, y, z);
+            cube.matrix.scale(1, 1, 1);
+            this.scene.push(cube);
+          }
+
+        }
+      }
+    }
+
+    for (let z = -15; z < 15; z++) {
+      for (let x = -15; x < 15; x++) {
+        // Generate noise for the current x, y coordinates
+        const noiseValue = noise2D(x / 10, z / 10);
+        if (noiseValue >= 0) {
+          const height = Math.floor(noiseValue * 5);
+          for (let y = 0; y <= height; y++) {
+            let cube = new VoxelMiner.Cube(2, 2, gl);
+            cube.color = [0.49, 0.788, 0.29, 1.0];
+            cube.matrix.translate(x, y, z);
+            cube.matrix.scale(1, 1, 1);
+            this.scene.push(cube);
+          }
+        } else {
+          let cube = new VoxelMiner.Cube(2, 2, gl);
+          cube.color = [0.49, 0.788, 0.29, 1.0];
+          cube.matrix.translate(x, 0, z);
+          cube.matrix.scale(1, 1, 1);
+          this.scene.push(cube);
+        }
+      }
+    }
+
+    let bedrock = new VoxelMiner.Cube(2, 3, gl);
+    bedrock.color = [0.49, 0.788, 0.29, 1.0];
+    bedrock.matrix.translate(0, -15, 0);
+    bedrock.matrix.scale(-32, 0.01, -32);
+    this.scene.push(bedrock);
+  }
+
+  /**
+   * Adds a skybox to the scene graph.
+   */
+  addSkyBox() {
+    VoxelMiner.debugLog(debugkey, "Adding skybox to scene graph");
+    this.scene.skybox = new VoxelMiner.Skybox(gl, VoxelMiner.SkyBox_BlueCloud);
+  }
+
+  /**
+   * Adds lights to the scene graph.
+   */
+  addLights() {
+    VoxelMiner.debugLog(debugkey, "Adding lights to scene graph");
+    const light1 = new VoxelMiner.PointLight(gl, [15, 3, -15], obj.light1_color, 1);
+    this.scene.addLight(light1);
+  }
+
+  addCrosshair() {
+    this.scene.crosshair = new VoxelMiner.Crosshair(gl);
+  }
+}
